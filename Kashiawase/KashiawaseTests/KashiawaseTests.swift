5740fad0a344544ad2e1d2d90786227c40e187c3
mi-snow <mi131snow@namikimiyuki-no-MacBook-Air.local>
2016-10-29 20:53:40 +0900

    README.mdと/kashiawaseを追加

---
+++ Kashiawase/KashiawaseTests/KashiawaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  KashiawaseTests.swift
+//  KashiawaseTests
+//
+//  Created by mi-snow on 2016/10/29.
+//  Copyright © 2016年 KamihiraProject. All rights reserved.
+//
+
+import XCTest
+@testable import Kashiawase
+
+class KashiawaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
