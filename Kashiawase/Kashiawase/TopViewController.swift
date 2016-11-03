a82fff2f426cfb9213b4c3a6646d6b286d8b477a
mi-snow <mi131snow@namikimiyuki-no-MacBook-Air.local>
2016-11-03 15:06:13 +0900

    トップページ作成

--- Kashiawase/Kashiawase/TopViewController.swift
+++ Kashiawase/Kashiawase/TopViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TopViewController: UIViewController {
+    @IBOutlet weak var springBtn : UIButton!
+    @IBOutlet weak var summerBtn : UIButton!
+    @IBOutlet weak var autumnBtn : UIButton!
+    @IBOutlet weak var winterBtn : UIButton!
+    @IBOutlet weak var allSeasonsBtn : UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
