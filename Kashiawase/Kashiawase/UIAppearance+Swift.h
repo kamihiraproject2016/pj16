312beb4efdc350aea9fcac015de5dd4380b2ba7c
mi-snow <mi131snow@namikimiyuki-no-MacBook-Air.local>
2016-11-18 15:30:05 +0900

    seasonカラムの内容を変更しました

---
+++ Kashiawase/Kashiawase/UIAppearance+Swift.h
@@ -0,0 +1,13 @@
+//
+//  UIAppearance+Swift.h
+//  Kashiawase
+//
+//  Created by mi-snow on 2016/11/17.
+//  Copyright © 2016年 KamihiraProject. All rights reserved.
+//
+
+#ifndef UIAppearance_Swift_h
+#define UIAppearance_Swift_h
+
+
+#endif /* UIAppearance_Swift_h */
